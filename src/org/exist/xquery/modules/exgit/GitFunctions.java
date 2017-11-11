04b36fa9aa1c90cf5eb1cd793ac7633be536711e
Dario Kampkaspar <dariok@web.de>
2017-11-11 10:24:39 -0800

    should not give any build errors now

--- src/org/exist/xquery/modules/exgit/GitFunctions.java
+++ src/org/exist/xquery/modules/exgit/GitFunctions.java
@@ -3,9 +3,11 @@ package org.exist.xquery.modules.exgit;
 import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
-import org.exist.xquery.Dependency;
+//import org.exist.xquery.Dependency;
+import org.exist.xquery.ErrorCodes.ErrorCode;
+//import org.exist.xquery.Function;
 import org.exist.xquery.FunctionSignature;
-import org.exist.xquery.Profiler;
+//import org.exist.xquery.Profiler;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.DoubleValue;
@@ -34,8 +36,18 @@ public class GitFunctions extends BasicFunction {
 	}
 
 	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+		String functionName = getSignature().getName().getLocalPart();
+		
 		Sequence result;
-		result = new DoubleValue(0);
+		
+		switch (functionName) {
+		case "commit":
+			result = new DoubleValue(0);
+			break;
+		default:
+			throw new XPathException(new ErrorCode("E01", "not found"), "not found");
+		}
+		
 		return result;
 	}
 }
